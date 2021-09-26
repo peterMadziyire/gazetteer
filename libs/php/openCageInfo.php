6489b0f056a8c9830118bfb50dcf924df8f20340
peterMadziyire <90290219+peterMadziyire@users.noreply.github.com>
2021-09-26 23:32:17 +0100

    Update openCageInfo.php

--- libs/php/openCageInfo.php
+++ libs/php/openCageInfo.php
@@ -1,39 +1,5 @@
 <?php
-    // ini_set('display_errors', 'On');
-    // error_reporting(E_ALL);
-    // ini_set('memory_limit', '1024M');
 
-    // $executionStartTime = microtime(true);
-
-
-    // //OpenCage Routine
-
-    // $oc_url="https://api.opencagedata.com/geocode/v1/json?q=". $_REQUEST['lat']. "+".$_REQUEST['lng']."&key=200a79aaae3747c6ade4bd658a9800e9";
-  
-    // $oc_ch = curl_init();
-    // curl_setopt($oc_ch, CURLOPT_SSL_VERIFYPEER, false);
-	// curl_setopt($oc_ch, CURLOPT_RETURNTRANSFER, true);
-	// curl_setopt($oc_ch, CURLOPT_URL, $oc_url);
-
-    // $oc_result = curl_exec($oc_ch);
-
-    // curl_close($oc_ch)
-
-    // $oc_decode = json_decode($oc_result,true);
-    // // $openCage= null;
-    // // $openCage['iso3'] = $oc_decode['results'][0];
-
-    // $output['status']['code'] = "200";
-    // $output['status']['name'] = "ok";
-    // $output['status']['description'] = "success";
-    // $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-    // $output['data'] = $oc_decode['results'];
-
-
-
-    // header('Content-Type: application/json; charset=UTF-8');
-
-    // echo json_encode($output);
 
     // Echo all errors back to the screen of the browser so PHP can be debugged
     ini_set('display_errors', 'On');
